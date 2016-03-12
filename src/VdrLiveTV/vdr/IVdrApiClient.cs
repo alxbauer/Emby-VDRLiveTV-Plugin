1cdcfa29f54ed32caa36c2739097497bc5fa3404
Alexander Bauer <alexander.bauer@reddoxx.com>
2016-03-12 17:45:55 +0100

    Initial commit

---
+++ src/VdrLiveTV/vdr/IVdrApiClient.cs
@@ -0,0 +1,23 @@
+﻿using MediaBrowser.Model.LiveTv;
+using MediaBrowser.Controller.LiveTv;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VdrLiveTV.vdr
+{
+    public interface IVdrApiClient
+    {
+        Task<List<ChannelInfo>> GetChannelsAsync();
+        Task<List<ProgramInfo>> GetEventAsync(string channelId, DateTime startDateUtc, DateTime endDateUtc);
+        Task<byte[]> GetEventImageAsync(int evendId, int imageNumber);
+        Task<List<RecordingInfo>> GetRecordingsAsync();
+        Task DeleteRecording(string recordingId);
+        Task<List<TimerInfo>> GetTimersAsync();
+        Task CreateTimer(TimerInfo info);
+        Task UpdateTimer(TimerInfo info);
+        Task CancelTimer(string timerId);
+    }
+}
